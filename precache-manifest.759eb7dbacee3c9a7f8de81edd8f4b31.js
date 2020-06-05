1e8af85b416f38bc4c17e89ea96d13efc23c4bd4
isadoraramirez <mydemoni@hotmail.com>
2020-06-04 21:16:44 -0500

    Updates

---
+++ precache-manifest.759eb7dbacee3c9a7f8de81edd8f4b31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50efa040f914df5aed9094d9aed6fae5",
+    "url": "/busca-poster/index.html"
+  },
+  {
+    "revision": "bde02205960c1e46af26",
+    "url": "/busca-poster/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbf3d26714b8b9c27158",
+    "url": "/busca-poster/static/js/2.ad4803ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/busca-poster/static/js/2.ad4803ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde02205960c1e46af26",
+    "url": "/busca-poster/static/js/main.83da7bb5.chunk.js"
+  },
+  {
+    "revision": "147c8a3628175cade15e",
+    "url": "/busca-poster/static/js/runtime-main.da048adf.js"
+  }
+]);
